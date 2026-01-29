7bf6cb9d13652931544ac8f5aa21ae9951535fae
aryancodes12 <9040aryangupta@gmail.com>
2026-01-29 22:59:34 +0530

    Final UI

--- user_registration.py
+++ user_registration.py
@@ -1,8 +1,12 @@
 import json
+import time
+import os
 from rich.console import Console
 from rich.panel import Panel
 from rich.table import Table
+from rich.progress import track
 
+os.system("clear")
 console = Console()
 #Variable
 users = {}
@@ -13,9 +17,10 @@ try:
     with open(file_name) as f:
         users = json.load(f)
 except FileNotFoundError:
-    console.print("File does not exist", style="bold red")
+    pass
 else:
-    print("Previous data is loaded")
+    with console.status("Loading previous Data....", spinner = "moon"):
+        time.sleep(1)
 
 #Tracking the current user id from the JSON file
 if users:
@@ -47,33 +52,39 @@ def register_user():
     global counter
 
 #Name
-    first_name = input("Enter your first name: ").title()
-    last_name = input("Enter your last name: ").title()
+    console.print("Enter your First Name", style = "cyan")
+    first_name = input(">>> ").title()
+    console.print("\nEnter your Last Name", style = "cyan")
+    last_name = input(">>> ").title()
     full_name = f"{first_name} {last_name}"
 
 #Email
     while True:
-        email = input("Enter Email Id: ")
+        console.print("\nEnter Email Id", style = "cyan")
+        email = input(">>> ")
         if is_valid_email(email):
             break
         error("Invalid email")
 
 #Username
     while True:
-        username = input("Create username: ")
+        console.print("\nCreate Username", style = "cyan")
+        username = input(">>> ")
         if is_unique_username(username):
             break
         warn("Username already taken")
 
 #Password
     while True:
-        password = input("Create a password: ")
+        console.print("\nCreate a Password", style = "cyan")
+        password = input(">>> ")
         if password == username:
             warn("Password cannot match the username")
         elif len(password) < 8:
             error("Password must be of 8 character or more")
         else:
-            confirm = input("Confirm the password: ")
+            console.print("\nConfirm the Password", style = "cyan")
+            confirm = input(">>> ")
             if confirm == password:
                 break
             error("Password does not match")
@@ -88,25 +99,26 @@ def register_user():
         }
 
     counter += 1
-    console.print(
-        Panel("Registration Successful!", style = "bold green")
-        )
+    console.print("Registration Successful!", style = "bold green")
 
 
 #MAIN LOOP
 while True:
+    os.system("clear")
+    console.rule("[bold blue]Main Menu")
     console.print(
-        Panel("User Registration System", style = "Bold Cyan")
+        Panel("👤 User Registration System", style = "Bold Cyan")
         )
     console.print("\n[bold yellow]Menu[/bold yellow]")
-    
     console.print("1. Register User")
     console.print("2. Exit")
-    choice = input(">>>").lower()
+    choice = input(">>> ").lower()
     if choice == "1":
         register_user()
     elif choice == "2":
-        error("\nExiting the Program")
+        console.print(
+            Panel("👋 Thanks for using the System!", style = "bold red")
+            )
         break
     else:
         warn("Invalid Choice")
@@ -116,30 +128,25 @@ while True:
 with open(file_name, "w") as f:
     json.dump(users, f, indent = 4)
 
-console.print("\nData saved successfully", style = "Bold Green")
+with console.status("Saving Data ....", spinner = "moon"):
+    time.sleep(1)
+success("Data Saved")
 
 #DATA DISPLAY
-#for user, info in users.items():
-   # console.print(user, style = "Purple")
-   # for k, v in info.items():
-    #    console.print(f"{k} : {v}")
-   # print()
-
+console.rule("[bold yellow] User's Information")
 table = Table(title = "Registered Users")
 
 table.add_column("User ID", style = "cyan")
-table.add_column("Name")
-table.add_column("Email")
-table.add_column("Username")
-table.add_column("Password")
+table.add_column("Name", style = "green")
+table.add_column("Email", style = "yellow")
+table.add_column("Username", style = "magenta")
 
 for  uid, info in users.items():
     table.add_row(
         uid,
         info["Name"],
         info["Email"],
-        info["Username"],
-        info["Password"]
+        info["Username"]
      )
 
 console.print(table)
