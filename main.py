085901fada1dca40e99b8a7159588ebeb0f338cf
aryancodes12 <9040aryangupta@gmail.com>
2026-01-31 23:41:38 +0530

    style: Adding Panels

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import os
 from auth.storage import load_users, save_users
 from auth.register import register_user
-from auth.ui import console, success, warn, error, loading
+from auth.ui import console, success, warn, error, loading, panel
 
 os.system("clear") # for linux
 #os.system("cls") # for windows
 
+loading("Loading ....")
 users = load_users()
 if users:
     counter = max(int(u[1:]) for u in users.keys()) + 1
@@ -13,6 +14,7 @@ else:
     counter = 1
 
 while True:
+    panel("Authentication System")
     console.rule("Main Menu", style = "bold blue")
     console.print("1. Register User")
     console.print("2. Exit")
