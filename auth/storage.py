a46d2ca44d0aca7f4f5951f6fa974aef4ec11c5f
aryancodes12 <9040aryangupta@gmail.com>
2026-01-31 23:27:41 +0530

    Refactor: move the I/O logic into dedicated storage module

---
+++ auth/storage.py
@@ -0,0 +1,14 @@
+import json
+
+FILE = "data/user_data.json"
+
+def load_users():
+    try:
+        with open(FILE) as f:
+           return json.load(f)
+    except FileNotFoundError:
+        return {}
+
+def save_users(users):
+    with open(FILE, "w") as f:
+        json.dump(users, f, indent = 4)
