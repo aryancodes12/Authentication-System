8f9c597be64c5634a2fe3023129c98fafd7107c9
aryancodes12 <9040aryangupta@gmail.com>
2026-02-04 21:41:50 +0530

    feat: adding username and password match

--- auth/validators.py
+++ auth/validators.py
@@ -3,3 +3,10 @@ def is_valid_email(email):
 
 def is_unique_username(username, users):
     return username not in [u["Username"] for u in users.values()]
+
+
+def is_username_exist(username, users):
+    return username in [u["Username"] for u in users.values()]
+
+def is_pass_matched(password, users):
+    return password in [u["Password"] for u in users.values()]
