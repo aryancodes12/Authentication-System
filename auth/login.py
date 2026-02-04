84f95f91ae760d96fed0160c1e85b067899b9393
aryancodes12 <9040aryangupta@gmail.com>
2026-02-04 21:40:46 +0530

    Feat: Laying the basic logic for the system

--- auth/login.py
+++ auth/login.py
@@ -1,7 +1,25 @@
-from auth import load_users, save_users 
+ 
 from .ui import console, panel, success, warn, error, status, rule
+from .validators import is_username_exist, is_pass_matched
 
 def login(users):
+    panel("Welcome to the Login Page")
+    while True:
+        console.print("\nEnter your username or email")
+        username = input(">>> ").lower()
+        if is_username_exist(username, users):
+            success("Username exist")
+            while True:
+                console.print("\nEnter password")
+                password = input(">>> ")
+                if is_pass_matched(password, users):
+                    panel(f"Welcome back, {username}")
+                    break
+                else:
+                    error("Incorrect password or username")
+            break
+        else:
+            error("Username not found")
 
 
 
