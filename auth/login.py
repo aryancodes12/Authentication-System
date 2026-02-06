e0980abd2457954cb39d4c0f3b78fa377a4ad4f9
aryancodes12 <9040aryangupta@gmail.com>
2026-02-06 20:18:35 +0530

    feat: menu revert option

--- auth/login.py
+++ auth/login.py
@@ -1,14 +1,15 @@
- 
 from .ui import console, panel, success, warn, error, status, rule
-from .validators import is_username_exist, is_pass_matched
+from .validators import is_username_exist, is_email_exist, is_pass_matched
 
 def login(users):
+    rule("Login Page")
     panel("Welcome to the Login Page")
     while True:
-        console.print("\nEnter your username or email")
+        console.print("\nEnter your username or email ('m' for Menu)")
         username = input(">>> ").lower()
-        if is_username_exist(username, users):
-            success("Username exist")
+        if username == "q":
+            break
+        if is_username_exist(username, users) or is_email_exist(username, users):
             while True:
                 console.print("\nEnter password")
                 password = input(">>> ")
@@ -20,6 +21,5 @@ def login(users):
             break
         else:
             error("Username not found")
-
-
+    return username
 
