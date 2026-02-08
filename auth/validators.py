3a874844c273adf37352d917d9fd357f8fe4eaa3
aryancodes12 <9040aryangupta@gmail.com>
2026-02-08 22:24:34 +0530

    fix: removed some funtions

--- auth/validators.py
+++ auth/validators.py
@@ -3,12 +3,3 @@ def is_valid_email(email):
 
 def is_unique_username(username, users):
     return username not in [u["Username"] for u in users.values()]
-
-def is_username_exist(username, users):
-    return username in [u["Username"] for u in users.values()]
-
-def is_email_exist(username, users):
-    return username in [e["Email"] for e in users.values()]
-
-def is_pass_matched(password, users):
-    return password in [p["Password"] for p in users.values()]
