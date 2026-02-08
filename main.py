e30a9b1847e66ae4a45cb38b3d58b5786b4c03c8
aryancodes12 <9040aryangupta@gmail.com>
2026-02-08 22:25:10 +0530

    feat: changes

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 from auth.storage import load_users, save_users
 from auth.register import register_user
 from auth.login import login
-from auth.ui import console, success, warn, error, status, panel, table, rule
+from auth.ui import console, success, warn, error, status, panel, display_user_table, rule, page
 
 
 status("Loading ....")
@@ -13,19 +13,21 @@ else:
     counter = 1
 
 while True:
-    # os.system("clear") # for linux
-    #os.system("cls") # for windows
-    panel("Authentication System", "blue")
-    rule("Main Menu")
-    console.print("1. Register User")
-    console.print("2. Login")
-    console.print("3. Exit")
+    page()
+    panel("Authentication System")
+    rule("")
+    menu = """
+    1. Register New User
+    2. Login
+    3. Exit
+        """
+    panel(menu, title = "Menu")
     choice = input(">>> ")
 
     if choice == "1":
         register_user(users, counter)
     elif choice == "2":
-        login(users)
+        user = login(users)
     elif choice == "3":
         panel("👋 Thanks for using the System!")
         break
@@ -35,7 +37,7 @@ while True:
 save_users(users)
 status("Saving Data ....")
 
-panel("Data Saved", "green")
+panel("Data Saved", style = "green")
 
 status("Displaying Data ....")
-console.print(table(users))
+console.print(display_user_table(users))
