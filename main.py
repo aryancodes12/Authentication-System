c2827c47f7e28af8d7e3491f43c2c91faa04f7a1
aryancodes12 <9040aryangupta@gmail.com>
2026-02-08 22:40:42 +0530

    feat: connected dashboard and session

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import os
 from auth.storage import load_users, save_users
 from auth.register import register_user
 from auth.login import login
-from auth.ui import console, success, warn, error, status, panel, display_user_table, rule, page
+from auth.ui import console, success, warn, error, status, panel, display_user_table, rule, page, dashboard_menu
+from auth.sessions import start_session, end_session, get_current_user, is_logged_in
 
 
 status("Loading ....")
@@ -28,6 +29,25 @@ while True:
         register_user(users, counter)
     elif choice == "2":
         user = login(users)
+        if user:
+            start_session(user)
+
+            while is_logged_in():
+                current = get_current_user()
+
+                dashboard_menu(current)
+                dash_choice = input(">>> ")
+
+                if dash_choice == "1":
+                    panel(f"Name: {current['Name']}\n" f"Email: {current['Email']}\n"
+                    f"Username: {current['Username']}")
+
+                elif dash_choice == "2":
+                    end_session()
+                    success("Logged Out successfully")
+
+                else:
+                    error("Invalid choice")
     elif choice == "3":
         panel("👋 Thanks for using the System!")
         break
