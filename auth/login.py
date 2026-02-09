4a55333ba82929c647302987283450afa09bfcd7
aryancodes12 <9040aryangupta@gmail.com>
2026-02-09 17:58:50 +0530

    style: fixing ui

--- auth/login.py
+++ auth/login.py
@@ -2,11 +2,13 @@ import os
 from .ui import console, panel, success, warn, error, status, rule, page
 
 def login(users):
+    panel("Authentication System")
+    page()
     rule("Login Page")
     panel("Welcome to Login Page")
 
     while True:
-        console.print("\nEnter your username or email ('q' to quit)")
+        console.print("\nEnter your username or email ('q' to quit)", style="cyan")
         identifier = input(">>> ").lower()
 
         if identifier == "q":
@@ -21,11 +23,11 @@ def login(users):
             error("User not Found")
             continue
         for i in range(3, 0, -1):
-            console.print("\nEnter password")
-            password = input(">>> ")
+            console.print("\nEnter password", style="cyan")
+            password = input(">>> \n")
 
             if password == user["Password"]:
-                panel(f"Welcome back, {user['Name']}", "green")
+                panel(f"Welcome back, {user['Name']}", style="green")
                 return user
             else:
                 error("Incorrect Password")
