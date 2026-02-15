35008b4fe3ab85b47161d66319050379535875fd
aryancodes12 <9040aryangupta@gmail.com>
2026-02-15 20:17:57 +0530

    UI done

--- auth/login.py
+++ auth/login.py
@@ -20,25 +20,32 @@ def login(users):
                 user = u
                 break
         if not user:
+            space()
             error_panel("User not Found")
+            space(2)
             wait_for_enter()
             return None
 
         #Password input
         attempts = 3
         while attempts > 0:
+            space()
             warn_panel(f"{attempts} attempts left")
             password = get_input("Password", password=True)
 
             if password == user["Password"]:
+                space(2)
                 success_panel(f"Welcome back, {user['Name']}!")
+                space()
                 status("Starting session...", 1)
                 return user
             else:
                 attempts -= 1
                 if attempts > 0:
+                    space()
                     error_panel(f"Incorrect password! {attempts} attempts remaining")
                 else:
+                    space()
                     error_panel("Too many failed attempts")
                     warn_panel("Returning to main menu..")
                     status("", 1)
