74cb0662c550010c70d631271c911bed579ab236
aryancodes12 <9040aryangupta@gmail.com>
2026-02-15 20:52:54 +0530

    fixing error

--- auth/register.py
+++ auth/register.py
@@ -5,18 +5,22 @@ import time
 def register_user(users, counter):
     clear_screen()
     header("Welcome to the Registration Page")
-
+    space(2)
     #Name input
+    
     first_name = get_input("Enter your First Name").title()
+    space()
     last_name = get_input("Enter your Last Name").title()
 
     full_name = f"{first_name} {last_name}"
 
     #Email input
     while True:
+        space()
         email = get_input("Enter Email id")
         if is_valid_email(email):
             break
+        space()
         error("Invalid Email")
 
     #Username Creation
@@ -26,6 +30,7 @@ def register_user(users, counter):
         if is_unique_username(username, users):
             break
         else:
+            space()
             warn_panel("Username already taken")
 
     #Password Creation
@@ -34,12 +39,13 @@ def register_user(users, counter):
         password = get_input("Create a password")
     
         is_valid, message = validate_password(password, username)
-    
+        console.print(message)
         if is_valid:
             space()
             confirm = get_input("Confirm password")
-        
+            
             if password == confirm:
+                space()
                 break
             else:
                 error("❌ Passwords don't match!")
@@ -57,6 +63,6 @@ def register_user(users, counter):
 
     #Success message
     success_panel("Registration Successful!")
-    sleep(0.8)
+    sleep(1.6)
 
     return users, counter + 1
