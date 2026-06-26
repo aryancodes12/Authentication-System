1f385ebde7e2c3967bb5b0be45bcaf13fa9d4204
Aryan Gupta <aryansynthh@gmail.com>
2026-06-26 17:51:28 +0530

    CREATED DATABASE when connection is made

--- database/connect.py
+++ database/connect.py
@@ -7,5 +7,23 @@ try:
     print(f"\nConnection Successfull!\n")
 
     cursor = conn.cursor()
+    query = """CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    username VARCHAR(255) NOT NULL UNIQUE,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    )"""
+    
+    cursor.execute(query)
+    print(f"\nTable Created Successfully!\n")
+
 except pymysql.MySQLError as e:
     print(f"\nError: {e}\n")
+
+finally:
+    if cursor:
+        cursor.close()
+    if conn:
+        conn.close()
