cd2c8aef46891abbd8b1b980571e778759ee0369
Aryan Gupta <aryansynthh@gmail.com>
2026-06-30 20:00:40 +0530

    show user info is working correctly with mysql

--- auth/dashboard.py
+++ auth/dashboard.py
@@ -3,11 +3,14 @@ from .theme import *
 from .sessions import *
 from .validators import validate_password
 from .storage import save_users
+from database.select import select_one_user_info
 
-def user_dashboard(user, users):
+def user_dashboard(user):
     global username
+    global user_info
+    user_info = select_one_user_info(user)
     clear_screen()
-    username = user['Username']
+    username = user_info['username']
     header("DASHBOARD")
     space()
 
@@ -26,15 +29,15 @@ def user_dashboard(user, users):
     dash_choice = get_choice("Select option (1-5)")
 
     if dash_choice == "1":
-        show_user_profile(user)
+        show_user_profile(user_info)
     elif dash_choice == "2":
-        update_user_profile_name(user, users)
+        update_user_profile_name(user_info)
         
     elif dash_choice == "3":
-        update_user_password(user, users)
+        update_user_password(user_info)
         status("Returning to dashboard", 1)
     elif dash_choice == "4":
-        delete_user_profile(user, users)
+        delete_user_profile(user_info)
     elif dash_choice == "5":
         end_session()
         status("\nLogging Out ...", 1)
@@ -52,20 +55,18 @@ def show_user_profile(user):
     space()
     wait_for_enter("Press Enter to go back")
 
-        
 
-def update_user_profile_name(user, users):
+def update_user_profile_name(user):
     clear_screen()
     header("Update Name")
-    old_name = user["Name"]
+    old_name = user["name"]
     space()
     info(f"Current Name: {old_name}")
 
     new_name = get_input("Enter new name to update")
     space()
     if new_name:
-        user['Name'] = new_name
-        save_users(users)
+        user['name'] = new_name
         space()
         success_panel(f"Name updated from {old_name} to {new_name}")
         space()
@@ -81,7 +82,7 @@ def update_user_password(user, users):
         current = get_input("Enter current password")
         space()
 
-        if current == user['Password']:  
+        if current == user['password']:  
             password = get_input("Create a password")
             space()
             is_valid, message = validate_password(password, current)
@@ -92,7 +93,7 @@ def update_user_password(user, users):
                 confirm = get_input("Confirm password")
             
                 if password == confirm:
-                    user['Password'] = password
+                    user['password'] = password
                     save_users(users)
                     space()
                     success_panel("Password is Updated")
@@ -110,16 +111,16 @@ def delete_user_profile(user, users):
     "By performing this action, you are permenently\n"
     "deleting your account. This action CANNOT be undone.\n\n"
     f"Account to be deleted:\n"
-    f"- Name: {user['Name']}\n"
-    f"- Username: {user['Username']}\n"
-    f"- Email: {user['Email']}\n",
+    f"- Name: {user['name']}\n"
+    f"- Username: {user['username']}\n"
+    f"- Email: {user['email']}\n",
     title="WARNING"
     )
     space()
 
     password = get_input("Enter password to verify", password=True)
 
-    if password != user['Password']:
+    if password != user['password']:
         space()
         error_panel("Incorrect password!\nDeletion cancelled for security.")
         sleep(2)
@@ -142,11 +143,11 @@ def delete_user_profile(user, users):
         sleep(1.5)
         return
     
-    username = user['Username']
+    username = user['username']
     user_id_to_delete = None
 
     for uid, user_data in users.items():
-        if user_data['Username'] == username:
+        if user_data['username'] == username:
             user_id_to_delete = uid
             break
     
