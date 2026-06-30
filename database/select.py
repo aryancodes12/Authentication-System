ee8a158df8044c5aa5b0e02c80a89ec18a23531c
Aryan Gupta <aryansynthh@gmail.com>
2026-06-30 17:37:43 +0530

    Created new fucn is_correct_pass to check for users password

--- database/select.py
+++ database/select.py
@@ -1,7 +1,7 @@
 import pymysql
 from .get_db import get_db
 
-#SELECTING ALL USERS TO DISPLAY TO ADMIN
+#Fetching all users
 def select_all_users():
     try:
         conn = cursor = None
@@ -66,6 +66,28 @@ def is_email_exists(email):
 
         if row:
             return True
+        return False
+    
+    except pymysql.MySQLError as e:
+        print(e)
+    finally:
+        if cursor: cursor.close()
+        if conn: conn.close()
+
+#Fetching to verify password entered by user
+def is_correct_pass(username):
+    try:
+        conn = cursor = None
+        conn = get_db()
+        cursor = conn.cursor()
+
+        query = "SELECT password FROM users WHERE username = %s"
+
+        cursor.execute(query, (username),)
+        row = cursor.fetchone()
+
+        if row:
+            return row
         return False
     
     except pymysql.MySQLError as e:
