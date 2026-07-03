016e31b5836abd1d32d5b928349726db607f4386
aryancodes12 <aryansynthh@gmail.com>
2026-07-03 10:18:21 +0530

    created 2 func, 1st for database and 2nd for user's table

--- database/connect.py
+++ database/connect.py
@@ -1,29 +1,43 @@
 import pymysql
-from .get_db import get_db
+from .get_db import get_db, get_db_database_connection
 
-try:
-    conn = cursor = None
-    conn = get_db()
-    print(f"\nConnection Successfull!\n")
+def create_database():
+    try:
+        conn = get_db_database_connection()
+        cursor = conn.cursor()
+        cursor.execute("CREATE DATABASE IF NOT EXISTS auth_server")
 
-    cursor = conn.cursor()
-    query = """CREATE TABLE IF NOT EXISTS users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    username VARCHAR(255) NOT NULL UNIQUE,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-    )"""
+        cursor.execute("USE auth_server")
+    except pymysql.MySQLError as e:
+        print(f"\nError: {e}\n")
+    finally:
+        if cursor:
+            cursor.close()
+        if conn:
+            conn.close()
+
+def create_users_table():
+    try:
+        conn = cursor = None
+        conn = get_db()
+
+        cursor = conn.cursor()
+        query = """CREATE TABLE IF NOT EXISTS users (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        username VARCHAR(255) NOT NULL UNIQUE,
+        email VARCHAR(255) NOT NULL UNIQUE,
+        password VARCHAR(255) NOT NULL,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        )"""
     
-    cursor.execute(query)
-    print(f"\nTable Created Successfully!\n")
+        cursor.execute(query)
 
-except pymysql.MySQLError as e:
-    print(f"\nError: {e}\n")
+    except pymysql.MySQLError as e:
+        print(f"\nError: {e}\n")
 
-finally:
-    if cursor:
-        cursor.close()
-    if conn:
-        conn.close()
+    finally:
+        if cursor:
+            cursor.close()
+        if conn:
+            conn.close()
