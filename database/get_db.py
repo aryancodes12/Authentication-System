79dc677f37c1650bcad02e1661ab1070fb311c86
aryancodes12 <aryansynthh@gmail.com>
2026-07-03 10:17:35 +0530

    updating so didn't have to create database manually

--- database/get_db.py
+++ database/get_db.py
@@ -1,5 +1,14 @@
 import pymysql
-from .config import DB
+from .config import DB, DB_without_database
+
+def get_db_database_connection():
+    return pymysql.connect(
+        host = DB_without_database['host'],
+        user = DB_without_database['user'],
+        password = DB_without_database['password'],
+        cursorclass = pymysql.cursors.DictCursor,
+        autocommit = True
+    )
 
 def get_db():
     return pymysql.connect(
